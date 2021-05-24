dd0a44bf705413eabae5515d0e895fad4937a2f2
Alessandra Ogawa <alessandrayco@sl.insper.edu.br>
2021-05-24 17:54:47 -0300

    added barrels (v1)

--- src/assets/assets.py
+++ src/assets/assets.py
@@ -9,4 +9,6 @@ def load_assets():
     player = pygame.image.load('src/assets/images/sergio.png').convert_alpha()
     assets['background'] = pygame.image.load('src/assets/images/background.png').convert()
     assets['player'] = pygame.transform.scale(player, (150, 150))
+    barrel = pygame.image.load('src/assets/images/barril.png').convert_alpha()
+    assets['barrel'] = pygame.transform.scale(barrel, (100, 100))
     return assets
--- src/screens/TelaJogo.py
+++ src/screens/TelaJogo.py
@@ -13,6 +13,20 @@ from assets import load_assets
 pygame.init()
 TELA = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Barrel(pygame.sprite.Sprite):
+    def __init__(self, assets):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets['barrel']
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = 1350
+        self.rect.y = 380
+        self.speedx  = -3
+    
+    def update(self):
+        self.rect.x += self.speedx
+
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, assets, posx):
@@ -24,16 +38,22 @@ class Player(pygame.sprite.Sprite):
         self.rect.centery = 429
         self.rect.x = posx
         self.scale = 3
-        self.assets = assets
        
     # Método update
 
+
 all_sprites = pygame.sprite.Group()
+all_barrels = pygame.sprite.Group()
+groups = {}
+groups['all_sprites'] = all_sprites 
+
+
 def tela_jogo(screen):
-    
-    groups = {}
-    groups['all_sprites'] = all_sprites
     assets = load_assets()
+    
+    barrel1 = Barrel(assets)
+    all_barrels.add(barrel1)
+    
 
     all_sprites.add(Player(assets, 250))
     lista_sprites = [all_sprites]
@@ -46,9 +66,14 @@ def tela_jogo(screen):
     background_rect = background.get_rect()
 
     estado = game
-
+    barrel1.speedx
     while estado:
         clock.tick(FPS)
+        if barrel1.rect.x < 400:
+            barrel2 = Barrel(assets)
+
+            all_barrels.add(barrel2)
+
         # ----- Trata eventos
         for event in pygame.event.get():
             # ----- Verifica consequências
@@ -67,7 +92,9 @@ def tela_jogo(screen):
         screen.blit(background, background_rect2)
         
         all_sprites.update()
+        all_barrels.update()
         all_sprites.draw(TELA)
+        all_barrels.draw(TELA)
         pygame.display.update()
         
 
